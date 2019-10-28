7e49380379c395bb27f258eef125e2748d7d1983
Casandra <casandragrullon@pursuit.org>
2019-10-28 18:07:23 -0400

    finished all requirements

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -10,11 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var winOrLost: UILabel!
+    
+    
+    @IBOutlet weak var card1: UIButton!
+    
+    
+    @IBOutlet weak var card2: UIButton!
+    
+    
+    @IBOutlet weak var card3: UIButton!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-
+  
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        card1.setBackgroundImage(UIImage(named: "cardBackRed"), for: .normal)
+        card2.setBackgroundImage(UIImage(named: "cardBackRed"), for: .normal)
+        card3.setBackgroundImage(UIImage(named: "cardBackRed"), for: .normal)
+        card1.isEnabled = true
+        card2.isEnabled = true
+        card3.isEnabled = true
+    }
+    
+    
+    @IBAction func card(_ sender: UIButton) {
+        let images = ["threeCard" , "kingCard" , "threeCard"]
+        let random = images.randomElement() ?? "threeCard"
+        
+        switch sender.tag {
+        case 0 :
+            sender.setBackgroundImage(UIImage(named: random), for: .normal)
+            if random == "kingCard" {
+             winOrLost.text = "you won!"
+            } else {
+                winOrLost.text = "you lost!"
+            }
+            
+        case 1 :
+        sender.setBackgroundImage(UIImage(named: random), for: .normal)
+        if random == "kingCard" {
+         winOrLost.text = "you won!"
+        } else {
+            winOrLost.text = "you lost!"
+        }
+        case 2 :
+        sender.setBackgroundImage(UIImage(named: random), for: .normal)
+        if random == "kingCard" {
+         winOrLost.text = "you won!"
+        } else {
+            winOrLost.text = "you lost!"
+        }
+        default :
+        sender.setBackgroundImage(UIImage(named: random), for: .normal)
+        if random == "kingCard" {
+         winOrLost.text = "you won!"
+        } else {
+            winOrLost.text = "you lost!"
+        }
+        }
+        card1.isEnabled = false
+        card2.isEnabled = false
+        card3.isEnabled = false
+        
+        
+        
+        
+    }
+    
+    
 }
 
